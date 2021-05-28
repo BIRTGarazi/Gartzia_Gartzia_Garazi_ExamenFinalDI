90292a3f0a421dd1e29c0e72e38447fe723a1547
Garazi Gartzia <ggartzia@birt.eus>
2021-05-28 19:48:11 +0200

    First commit

---
+++ DataAccessLayer/AccesoDatosDAL.cs
@@ -0,0 +1,172 @@
+﻿using ObjetosTransferencia.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DataAccessLayer
+{
+    public static class AccesoDatosDAL
+    {
+        #region Atributos
+
+        private static String consultaSQL;
+
+        /// <summary>
+        ///  Cadena de conexion
+        /// </summary>
+        private static SqlConnection conexion = new SqlConnection("Data Source=localhost;Initial Catalog=Northwind;User ID=usuarioDI;Password=1234");
+
+        /// <summary>
+        /// lista de clientes
+        /// </summary>
+        private static List<ClienteDTO> listaClientes = new List<ClienteDTO>();
+
+        /// <summary>
+        /// Lista de pedidos de un cliente concreto
+        /// </summary>
+        private static List<PedidoDTO> listaPedidos = new List<PedidoDTO>();
+
+        #endregion
+
+        #region Constructores
+
+        #endregion
+
+        #region Propiedades
+
+        public static string ConsultaSQL { get => consultaSQL; set => consultaSQL = value; }
+        public static List<PedidoDTO> ListaPedidos { get => listaPedidos; set => listaPedidos = value; }
+        public static List<ClienteDTO> ListaClientes { get => listaClientes; set => listaClientes = value; }
+
+
+        #endregion
+
+        #region Metodos
+
+        /// <summary>
+        /// Consultar datos en una base de datos SQLServer 
+        /// </summary>
+        public static List<ClienteDTO> ListadoClientesNorthWind()
+        {
+            return RealizarConsultaClientes("select * from dbo.Customers");
+        }
+
+        /// <summary>
+        /// Pide a la base de datos la lista de pedidos para un cliente concreto
+        /// </summary>
+        /// <param name="indiceCliente"></param>
+        /// <returns></returns>
+        public static List<PedidoDTO> ListadoPedidosCliente(String nombreCliente)
+        {
+            return RealizarConsultaPedidos("select * from dbo.Orders WHERE CustomerID = '" + nombreCliente + "'");
+        }
+
+
+
+        /// <summary>
+        /// Realizar consulta a la BD
+        /// </summary>
+        private static List<ClienteDTO> RealizarConsultaClientes(String consulta)
+        {
+            SqlCommand command;
+
+            // Objeto para elctura de datos
+            SqlDataReader dataReader;
+
+            try
+            {
+                conexion.Open();
+
+                command = new SqlCommand(consulta, conexion);
+
+                dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    object[] values = new object[11];
+                    dataReader.GetValues(values);
+
+                    ClienteDTO cliente = new ClienteDTO(values[0].ToString(), values[1].ToString());
+                    listaClientes.Add(cliente);
+                }
+
+                dataReader.Close();
+                command.Dispose();
+
+                return listaClientes;
+
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception("No se ha podido estabecer la conexion con la BD!" + e.Message);
+
+            }
+            finally
+            {
+                conexion.Close();
+
+            }
+
+        }
+
+
+        /// <summary>
+        /// Realizar consulta a la BD
+        /// </summary>
+        private static List<PedidoDTO> RealizarConsultaPedidos(String consulta)
+        {
+            SqlCommand command;
+
+            // Objeto para elctura de datos
+            SqlDataReader dataReader;
+
+            try
+            {
+                conexion.Open();
+
+                command = new SqlCommand(consulta, conexion);
+
+                dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    object[] values = new object[14];
+                    dataReader.GetValues(values);
+
+                    int idPedido = int.Parse(values[0].ToString());
+                    string idCliente = values[1].ToString();
+                    DateTime fechaP = DateTime.Parse(values[3].ToString());
+                    DateTime fechaE = DateTime.Parse(values[4].ToString());
+                    double precio = double.Parse(values[7].ToString());
+                    string direccion = values[9].ToString() + " " + values[10].ToString();
+
+                    PedidoDTO pedido = new PedidoDTO(idPedido, idCliente, fechaP, fechaE, precio,  direccion);
+                    listaPedidos.Add(pedido);
+                }
+
+                dataReader.Close();
+                command.Dispose();
+
+                return listaPedidos;
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception("No se ha podido estabecer la conexion con la BD!" + e.Message);
+
+            }
+            finally
+            {
+                conexion.Close();
+
+            }
+
+        }
+
+
+        #endregion
+
+    }
+}
